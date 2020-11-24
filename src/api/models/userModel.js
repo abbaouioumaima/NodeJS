297ac2d6e5fb1fded34a676f9c94203b0e857eee
oumaima <abbaouioumaima1@gmail.com>
2020-11-24 17:58:17 +0100

    update

---
+++ src/api/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt')
+let userSchema = new Schema({
+    email: {
+        type: String,
+        required: "L'email est requis",
+        unique: true
+    },
+    password: {
+        type: String,
+        required: "Le mot de passe est requis"
+          
+    }
+})
+
+userSchema.pre('save',async function(next){
+  try {
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword= await bcrypt.hash(this.password,salt)
+    this.password = hashedPassword
+    next()
+}catch(error){
+    next(error)
+}
+})
+
+
+module.exports = mongoose.model('User', userSchema);
