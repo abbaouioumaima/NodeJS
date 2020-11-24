53209c103c5cb94ddff2984cfeff092e78add65a
oumaima <abbaouioumaima1@gmail.com>
2020-11-24 12:58:46 +0100

    Init du projet

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const server = express();
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://mongo/apinodejs')
+// moongoose.connect('mongodb://localhost:27017/apinodejs');  // Without docker
+
+const bodyParser = require('body-parser');
+server.use(bodyParser.urlencoded());
+server.use(bodyParser.json());
+
+const postRoute = require('./api/routes/postRoute');
+postRoute(server);
+
+const commentRoute = require('./api/routes/commentRoute');
+commentRoute(server);
+
+server.listen(port, hostname);
